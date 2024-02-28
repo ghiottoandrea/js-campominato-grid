e5c006b17d6cae1203aca0410d0710807f9f438c
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-28 16:30:56 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+
+//- Creo un pulsante e al click gli assegno la classe active per generare una tabella
+//- Creo una tabella di 10 righe fino a 100
+//- Al click della casella cambia colore e manda un massaggio in console con il numero
