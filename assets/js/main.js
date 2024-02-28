81573ee5cbcf23ceb0b62a0f80468e8e26027d55
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-28 17:26:43 +0100

    add grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 
 //- Creo un pulsante e al click gli assegno la classe active per generare una tabella
-//- Creo una tabella di 10 righe fino a 100
-//- Al click della casella cambia colore e manda un massaggio in console con il numero
+
+
+//- Creo una tabella di 10 X 10
+const containerEl = document.getElementById(`container`)
+let numb = 0
+
+for (let i = 0; i < 100; i++) {
+    numb += 1
+    let markup = `<div class="box">${numb}</div>`
+    containerEl.insertAdjacentHTML(`beforeend`, markup)
+}
+
